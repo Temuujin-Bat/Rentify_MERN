6c67c25ca54b5775f41223a7aa9a1e231722049c
Temuujin <temujinba@gmail.com>
2024-07-06 21:52:22 +0300

    Fixed missing compression dependency

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ dotenv.config();
 const app = express();
 import cookieParser from "cookie-parser";
 import path from "path";
+import compression from "compression";
 
 import apartmentRouter from "./routes/apartmentRouter.js";
 import authRouter from "./routes/authRouter.js";
@@ -24,6 +25,7 @@ app.use(
 );
 app.use(cookieParser());
 app.use(morgan("dev"));
+app.use(compression());
 
 ///
 /// Routes
